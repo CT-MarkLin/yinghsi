40379a9ffdcb36b384e91199a54cd4460380216f
mark-lin <lzl0419@gmail.com>
2023-01-02 08:40:17 +0800

    init

--- common.mjs
+++ common.mjs
@@ -30,7 +30,7 @@ export async function getWujinList(page = 1) {
     }
     const result = {
       name: data1.textContent.replace(/\s/g, ""),
-      href: data1.href,
+      href: data1.href && data1.href.match(/id\/(\d+)\./)[1],
       type: data2.textContent,
       lastUpdateTime: data3.textContent,
     };
@@ -38,4 +38,17 @@ export async function getWujinList(page = 1) {
   });
   // console.log(data.href);
   // console.log(data.textContent);
+}
+
+export async function getWujindetail(id = "") {
+  const html = await axios.get(
+    `https://www.wujinzy.com/index.php/vod/detail/id/${id}.html`
+  );
+  // console.log(html.data);
+  // const { document } = new JSDOM(html.data).window;
+  const regLink = /\/>.+?\$https.+?</g;
+  const result = html.data.match(regLink) || [];
+  return result
+    .filter((item) => !item.includes(".m3u8"))
+    .map((item) => item.replace("/>", "").replace("<", "").split("$"));
 }
